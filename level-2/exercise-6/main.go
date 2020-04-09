e0e2ff7122537984764d2a4cbdd851326a163e58
Mindaugas Degutis <mindaugas@degutis.lt>
2020-04-09 09:54:22 +0300

    Ex6 from LVL2

---
+++ level-2/exercise-6/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const (
+		currentYear = 2020
+		a = currentYear + iota
+		b = currentYear + iota
+		c = currentYear + iota
+		d = currentYear + iota
+	)
+
+	fmt.Println(a, b, c, d)
+}
