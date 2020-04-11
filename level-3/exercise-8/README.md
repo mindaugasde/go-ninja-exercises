20b24d2d3dee0f2fff7b3c4f1e22ee0eeaca8ac3
Mindaugas Degutis <mindaugas@degutis.lt>
2020-04-12 00:58:25 +0300

    Ex8 from LVL3

---
+++ level-3/exercise-8/README.md
@@ -0,0 +1,3 @@
+```
+Create a program that uses a switch statement with no switch expression specified.
+```
---
+++ level-3/exercise-8/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch {
+	case true:
+		fmt.Println("yes, it's true")
+	case false:
+		fmt.Println("no way dude")
+	}
+}
