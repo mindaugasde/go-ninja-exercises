ff67407f46e6dce5f73ccf67281ef34b617fe350
Mindaugas Degutis <mindaugas@degutis.lt>
2020-04-18 01:42:12 +0300

    Ex10 from LVL4

---
+++ level-4/exercise-10/README.md
@@ -0,0 +1,3 @@
+```
+Using the code from the previous example, delete a record from your map. Now print the map out using the “range” loop
+```
---
+++ level-4/exercise-10/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james": {"Shaken, not stirred", "Martinis", "Women"},
+		"moneypenny_miss": {"James Bond", "Literature", "Computer Science"},
+		"no_dr": {"Being evil", "Ice cream", "Sunsets"},
+	}
+
+	m["mindaugas"] = []string{"F1", "Cars", "Programming"}
+	delete(m, "mindaugas")
+
+	for k, v := range m {
+		fmt.Println(k, v)
+		for i, vv := range v {
+			fmt.Println(i, vv)
+		}
+	}
+}
